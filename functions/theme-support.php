e74ec0acb750969a78d2b43e7fffbd7353698ac5
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-09-07 14:14:00 +0300

    added russian localisation

--- functions/theme-support.php
+++ functions/theme-support.php
@@ -2,6 +2,7 @@
 if (!function_exists('sensive_setup')) {
   function sensive_setup()
   {
+    load_theme_textdomain('sensive', get_template_directory() . '/languages');
     add_theme_support('custom-logo', [
       'height'      => 26,
       'width'       => 122,
--- functions/tours-post-type.php
+++ functions/tours-post-type.php
@@ -4,18 +4,18 @@ function tours_init()
 {
 	register_post_type('book', array(
 		'labels'             => array(
-			'name'               => __('Tours'), // Основное название типа записи
-			'singular_name'      => __('Tour'), // отдельное название записи типа Book
-			'add_new'            => __('Add new'),
-			'add_new_item'       => __('Add new tour'),
-			'edit_item'          => __('Edit tour'),
-			'new_item'           => __('New tour'),
-			'view_item'          => __('View tour'),
-			'search_items'       => __('Find tour'),
-			'not_found'          => __('Tours not found'),
-			'not_found_in_trash' => __('No tours in trash'),
+			'name'               => __('Tours', 'sensive'), // Основное название типа записи
+			'singular_name'      => __('Tour', 'sensive'), // отдельное название записи типа Book
+			'add_new'            => __('Add new', 'sensive'),
+			'add_new_item'       => __('Add new tour', 'sensive'),
+			'edit_item'          => __('Edit tour', 'sensive'),
+			'new_item'           => __('New tour', 'sensive'),
+			'view_item'          => __('View tour', 'sensive'),
+			'search_items'       => __('Find tour', 'sensive'),
+			'not_found'          => __('Tours not found', 'sensive'),
+			'not_found_in_trash' => __('No tours in trash', 'sensive'),
 			'parent_item_colon'  => '',
-			'menu_name'          => __('Tours')
+			'menu_name'          => __('Tours', 'sensive')
 
 		),
 		'public'             => true,
