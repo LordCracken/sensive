d50f4958b52acd5cea22a7cba2d500d06e97fd0c
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-09-11 17:02:28 +0300

    registered home & tours pages sidebars

--- functions/widgets.php
+++ functions/widgets.php
@@ -2,8 +2,30 @@
 function sensive_widgets_init()
 {
   register_sidebar(array(
+    'name'          => __('Home Sidebar', 'sensive'),
+    'id'            => 'sidebar_home',
+    'before_widget' => '',
+    'after_widget'  => '',
+    'before_sidebar' => '<div class="col-lg-4 sidebar-widgets %2$s"><div class="widget-wrap">',
+    'after_sidebar'  => '</div></div>',
+    'before_title'  => '',
+    'after_title'   => '',
+  ));
+
+  register_sidebar(array(
     'name'          => __('Blog Sidebar', 'sensive'),
     'id'            => 'sidebar_blog',
+    'before_widget' => '',
+    'after_widget'  => '',
+    'before_sidebar' => '<div class="col-lg-4 sidebar-widgets %2$s"><div class="widget-wrap">',
+    'after_sidebar'  => '</div></div>',
+    'before_title'  => '',
+    'after_title'   => '',
+  ));
+
+  register_sidebar(array(
+    'name'          => __('Tours Sidebar', 'sensive'),
+    'id'            => 'sidebar_tours',
     'before_widget' => '',
     'after_widget'  => '',
     'before_sidebar' => '<div class="col-lg-4 sidebar-widgets %2$s"><div class="widget-wrap">',
