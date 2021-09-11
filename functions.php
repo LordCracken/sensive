73d5a8fd09f6f0f5790370d407aa25f9795c2ab7
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-09-11 13:30:24 +0300

    created folder for custom widgets

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@ require('functions/styles-and-scripts.php');
 require('functions/theme-support.php');
 require('functions/menus.php');
 require('functions/post-types.php');
-require('functions/search-widget.php');
 require('functions/widgets.php');
+
+require('widgets/search-widget.php');
 
 remove_filter('the_content', 'wpautop');
