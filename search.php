e9ab6e59fc9bb13c28713339f76574231741b4c6
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-09-12 00:45:06 +0300

    added dynamic heading in search page; added pagination in search page

--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
-$hero_content = '<h1>Search: fast</h1>';
+$hero_content = '<h1>' . __('Search: ', 'sensive') . get_search_query() . '</h1>';
 get_header(null, ['content' => $hero_content]) ?>
 
 <!--================ Start Blog Post Area =================-->
@@ -8,182 +8,65 @@ get_header(null, ['content' => $hero_content]) ?>
     <div class="row">
       <div class="col-lg-8">
         <div class="row">
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card1.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
+          <?php
+          $current = absint(
+            max(
+              1,
+              get_query_var('paged') ? get_query_var('paged') : get_query_var('page')
+            )
+          );
+
+          $query = new WP_Query(['s' => get_search_query(), 'sentence' => true, 'paged' => $current, 'posts_per_page' => 8]);
+          if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+              <div class="col-md-6">
+                <div class="single-recent-blog-post card-view">
+                  <div class="thumb">
+                    <img class="card-img rounded-0" src="<?php echo has_post_thumbnail() ? the_post_thumbnail_url() : 'https://sensive.vladislav-yakimovskiy.ru/wp-content/uploads/2021/09/hero-banner.png' ?>" alt="">
+                    <ul class="thumb-info">
+                      <li><a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>"><i class="ti-user"></i><?php the_author() ?></a></li>
+                      <li><a href="<?php echo get_the_permalink() ?>/#comments"><i class="ti-themify-favicon"></i><?php comments_number(__('Empty', 'sensive'), __('1 Comment', 'sensive'), __('% Comments', 'sensive')) ?></a></li>
+                    </ul>
+                  </div>
+                  <div class="details mt-20">
+                    <a href="blog-single.html">
+                      <h3><?php the_title() ?></h3>
+                    </a>
+                    <p><?php the_excerpt() ?></p>
+                    <a class="button" href="#"><?php echo  __('Read More', 'sensive') ?> <i class="ti-arrow-right"></i></a>
+                  </div>
+                </div>
               </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Fast cars and rickety bridges as
-                    he grand tour returns</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card2.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Harvey Weinstein's senual assault
-                    trial set for May</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card3.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Fast cars and rickety bridges as
-                    he grand tour returns</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card4.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Harvey Weinstein's senual assault
-                    trial set for May</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card5.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Fast cars and rickety bridges as
-                    he grand tour returns</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card6.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Harvey Weinstein's senual assault
-                    trial set for May</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card7.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Fast cars and rickety bridges as
-                    he grand tour returns</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="single-recent-blog-post card-view">
-              <div class="thumb">
-                <img class="card-img rounded-0" src="<? echo get_template_directory_uri() ?>/img/blog/thumb/thumb-card8.png" alt="">
-                <ul class="thumb-info">
-                  <li><a href="#"><i class="ti-user"></i>Admin</a></li>
-                  <li><a href="#"><i class="ti-themify-favicon"></i>2 Comments</a></li>
-                </ul>
-              </div>
-              <div class="details mt-20">
-                <a href="blog-single.html">
-                  <h3>Harvey Weinstein's senual assault
-                    trial set for May</h3>
-                </a>
-                <p>Vel aliquam quis, nulla pede mi commodo no tristique nam hac luctus torquent velit felis lone commodo pellentesque</p>
-                <a class="button" href="#">Read More <i class="ti-arrow-right"></i></a>
-              </div>
-            </div>
-          </div>
+          <?php endwhile;
+          else : echo  __('No posts.', 'sensive');
+          endif ?>
         </div>
 
         <div class="row">
           <div class="col-lg-12">
-            <nav class="blog-pagination justify-content-center d-flex">
-              <ul class="pagination">
-                <li class="page-item">
-                  <a href="#" class="page-link" aria-label="Previous">
-                    <span aria-hidden="true">
-                      <i class="ti-angle-left"></i>
-                    </span>
-                  </a>
-                </li>
-                <li class="page-item active"><a href="#" class="page-link">1</a></li>
-                <li class="page-item"><a href="#" class="page-link">2</a></li>
-                <li class="page-item">
-                  <a href="#" class="page-link" aria-label="Next">
-                    <span aria-hidden="true">
-                      <i class="ti-angle-right"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <?php
+            global $wp_query;
+            $restore_wp_query = $wp_query;
+            $wp_query         = $query;
+            the_posts_pagination(array(
+              'before_page_number' => '<li class="page-item"><span class="page-link">',
+              'after_page_number'  => '</span></li>',
+              'prev_text'          => '<li class="page-item">
+                                          <span class="page-link" aria-label="Previous">
+                                            <span aria-hidden="true">
+                                              <i class="ti-angle-left"></i>
+                                            </span>
+                                          </span>
+                                        </li>',
+              'next_text'          => '<li class="page-item">
+                                          <span class="page-link" aria-label="Next">
+                                            <span aria-hidden="true">
+                                              <i class="ti-angle-right"></i>
+                                            </span>
+                                          </span>
+                                        </li>'
+            ));
+            $wp_query = $restore_wp_query;
+            ?>
           </div>
         </div>
       </div>
